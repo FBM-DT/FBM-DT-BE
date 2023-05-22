019afcda3293bfa1c4ceb1b1174967b4c97e65fe
Nam Pham <nam.pham@hats.solutions>
2023-05-22 23:13:25 +0700

    Integrate Postges and Sequelize

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
