b2bde010ac854deb4d4b2cae74dbb10a54fd9544
Nam Pham <nam.pham@hats.solutions>
2023-06-15 22:49:44 +0700

    Feat: [FD-35] setup Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('The FBM API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({transform: true}));
   app.enableCors();
   app.setGlobalPrefix('api/v1');
