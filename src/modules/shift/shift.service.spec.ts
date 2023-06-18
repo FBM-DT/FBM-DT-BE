5897d6bbb981eb4914144704c1ee6dd54f72e485
Vo Thanh <tthanhh410@gmail.com>
2023-06-18 22:00:00 +0700

    create/base-service

--- src/modules/shift/shift.service.spec.ts
+++ src/modules/shift/shift.service.spec.ts
@@ -1,13 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { ShiftService } from './shift.service';
 import { ShiftModule } from './shift.module';
-import { AddWorkShiftRequestDto } from './dto';
+import { AddWorkShiftRequestDto, AddWorkShiftResponseDto } from './dto';
 import { WORKTYPE } from '../../../src/core/constants';
 import { DatabaseModule } from '../../../src/db/database.module';
 
 describe('ShiftService', () => {
   let service: ShiftService;
-  
+
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       imports: [ShiftModule, DatabaseModule],
@@ -19,16 +19,27 @@ describe('ShiftService', () => {
 
   describe('Create new work shift', () => {
     it('should be created', async () => {
-      const result = [1];
+      const result: AddWorkShiftResponseDto = new AddWorkShiftResponseDto();
+      result.data = 1;
+      result.message = 'Success';
+      result.version = 'v1'
+      result.status = 201;
       const dto: AddWorkShiftRequestDto = {
         name: 'test',
         address: '55 Hardvard',
         duration: '1y',
         type: WORKTYPE.DAILY,
-        description: 'test'
-      }
-      jest.spyOn(service, 'createWorkShift').mockImplementation(async ()=> result[0])
-      expect(await service.createWorkShift(dto)).toBeCloseTo(1);
-    })
-  })
+        description: 'test',
+      };
+      jest
+        .spyOn(service, 'createWorkShift')
+        .mockImplementation(async () => result[0]);
+      expect(await service.createWorkShift(dto)).toEqual({
+        message: 'Success',
+        status: 201,
+        data: 1,
+        version: 'v1'
+      });
+    });
+  });
 });
